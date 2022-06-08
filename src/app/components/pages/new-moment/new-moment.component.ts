5cb39b8f620aef62f096d7fe9cf5501d295e97a1
AlanVidalll <alanfonseca7812@gmail.com>
2022-06-08 15:40:35 -0300

    Form crated moment implemented

--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Moment } from 'src/app/Moment';
+
+import { MomentService } from 'src/app/service/moment.service';
 
 @Component({
   selector: 'app-new-moment',
   templateUrl: './new-moment.component.html',
-  styleUrls: ['./new-moment.component.css']
+  styleUrls: ['./new-moment.component.css'],
 })
 export class NewMomentComponent implements OnInit {
+  btnText = 'Compatilhar!';
 
-  constructor() { }
+  constructor(private momentService: MomentService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  async createHandler(moment: Moment) {
+    const formData = new FormData();
+
+    formData.append('title', moment.title);
+    formData.append('description', moment.description);
+
+    if (moment.image) {
+      formData.append('image', moment.image);
+    }
+    // to do
+
+    await this.momentService.createMoment(formData).subscribe();
+
+    // enviar para o service
+
+    //exbir mensagem
+
+    //redirect
   }
-
 }
